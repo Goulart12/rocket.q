eb64cc0428ad6f0cd7d4649a1d8b0eec93efe9ce
Juarez Goulart <goulartgarcia12@gmail.com>
2021-06-24 02:07:56 -0300

    fourth commit

---
+++ src/route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/room', (req, res) => res.render("room"))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
+route.post('/room/:room/:question/:action', QuestionController.index)
+
+
+module.exports = route;
